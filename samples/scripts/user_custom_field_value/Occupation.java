bec8af38953c7993721354400061ccf047cb475b
Maxim Vasenkov <max.vasenkov@gmail.com>
2012-03-16 16:45:09 +0400

    comments added

--- samples/scripts/user_custom_field_value/Occupation.java
+++ samples/scripts/user_custom_field_value/Occupation.java
@@ -9,15 +9,29 @@ import com.trackstudio.external.UserUDFValueScript;
 import com.trackstudio.kernel.manager.KernelManager;
 import com.trackstudio.secured.SecuredTaskBean;
 import com.trackstudio.secured.SecuredUserBean;
-
+/**
+ * Этот скрипт предназначен для дополнительного поля типа Текст (с отображением в HTML), привязанного к пользователю<br>
+ * Он выводит список задач (со ссылками), в которых пользователь является ответственным.<br>
+ * Есть альтернативный способ вывода этой же информации: можно сделать дополнительное поле типа Задача, и в качестве значений выводить не String, а List<SecuredTaskBean>
+ * Приведенный здесь способ удобнее тем, что вместе со ссылкой на задачу можно выводить и другую информацию по вкусу.
+ * @author winzard
+ *
+ */
 public class Occupation implements UserUDFValueScript {
+/** Идентификатор фильтра, которым обрабатывается список задач.<br>
+ * Создайте свой фильтр и укажите здесь его идентификатор перед компиляцией.<br>
+ * В фильтре нужно указать опцию глубокого поиска. Можете также отсеять ненужные, например, закрытые задачи.
+ * */
 public static final String MY_FILTER="ff80818133205e4c01332163ae960575";
 	@Override
 	public Object calculate(SecuredUserBean user) throws GranException {
 		StringBuffer taskIds = new StringBuffer();
 		 TaskFilter taskList = new TaskFilter(new SecuredTaskBean("1", user.getSecure()));
-	        TaskFValue taskFValue = KernelManager.getFilter().getTaskFValue(MY_FILTER);
+		 // Загружаем параметры из указанного выше фильтра
+	     TaskFValue taskFValue = KernelManager.getFilter().getTaskFValue(MY_FILTER);
+	     // Меняем или устанавливаем в них параметр поиска по ответственному
 	       taskFValue.set(FieldMap.HUSER_NAME.getFilterKey(), user.getId());
+	       // Получаем список задач и запихиваем их через двойную решетку в выводимое поле. Обработчик затем преобразует всё это в ссылки
 	        ArrayList<SecuredTaskBean> taskCol = taskList.getTaskList(taskFValue, true, false, taskFValue.getSortOrder());
 	        if (taskCol!=null && taskCol.size()>0){
 	        	taskIds.append("<DIV>");
